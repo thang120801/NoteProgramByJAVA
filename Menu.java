c1caac88dec55e24c6dc655d0fb406ec5b12d1ad
Tran Cao Thang <100948592+thang120801@users.noreply.github.com>
2022-09-25 03:06:37 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("=====MENU===== \n");
+        System.out.println("1. Thêm note \n");
+        System.out.println("2. Mở note \n");
+        System.out.println("3. Xóa note \n");
+        System.out.println("4. Hiển thị danh sách toàn bộ note \n");
+        System.out.println("****************************************************************** \n");
+        System.out.println("Nhập lựa chọn của bạn: ");
+    }
+
+}
